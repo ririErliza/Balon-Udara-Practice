4b3dc13a3fa47b687f6cbab929ab58d07749292a
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-09-14 09:47:21 +0200

    background animate

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
 const balloon = document.getElementById('balloon');
 const gift = document.getElementById('gift');
+const canvas = document.getElementById('canvas');
+
+function setBackground(){
+    canvas.style.width = window.innerWidth + 'px';
+    canvas.style.height = window.innerHeight + 'px';
+    
+}
 
 function animate() {
     balloon
     .velocity({
-        left:'-500px',
-        
+        top: '0px',
+        left:'45%',
+        transform:["rotate(0deg)", "rotate(10deg)"]
     },{
-        duration: 17000,
+        duration: 7000,
+    }
+    )
+    .velocity({
+        left:'40%',
+    },{
+        duration: 7000,
+    }
+    )
+    .velocity({
+        left:'-500px',
+        transform:["rotate(0deg)", "rotate(0deg)"]
+    },{
+        duration: 15000,
     }
     )
 
@@ -16,14 +37,14 @@ function animate() {
     .velocity({
         opacity: 1,
     },{
-        delay: 4000,
+        delay: 13000,
         duration: 1000,
     })
     .velocity({
         top: window.innerHeight + 100,
     },{
         queue: false,
-        delay: 4000,
+        delay: 13000,
         duration: 9000,
     })
  
